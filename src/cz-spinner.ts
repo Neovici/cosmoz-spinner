9c2580ff050dc3cb6df532392af1b49c0adf03f1
John <codingjohn@outlook.com>
2025-08-27 11:53:28 +0800

    feat: initial implementation

---
+++ src/cz-spinner.ts
@@ -0,0 +1,28 @@
+import { component, html, css } from '@pionjs/pion';
+
+const styles = css`
+	@keyframes rotating {
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+
+	:host {
+		display: inline-block;
+		vertical-align: middle;
+		border-radius: 50%;
+		width: 22px;
+		height: 22px;
+		border: 2px solid rgba(0, 0, 0, 0.1);
+		border-top: 2px solid #5f5a92;
+		animation: rotating 1.2s infinite cubic-bezier(0.785, 0.135, 0.15, 0.86);
+		box-sizing: border-box;
+		margin: 0 4px;
+	}
+`;
+
+const Spinner = () => html``;
+
+export const CosmozSpinner = component(Spinner, { styleSheets: [styles] });
+
+customElements.define('cz-spinner', CosmozSpinner);
