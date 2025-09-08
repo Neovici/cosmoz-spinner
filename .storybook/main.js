567ccb6f53b706c599d469b5fe70afd939bc46fa
basilgood <elsile69@yahoo.com>
2025-09-08 14:53:51 +0300

    chore: bump storybook from 7.6.19 to 9.1.5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,10 @@
 export default {
 	stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-	addons: ['@storybook/addon-links', '@storybook/addon-docs'],
-
 	framework: {
 		name: '@storybook/web-components-vite',
 		options: {},
+	},
+	docs: {
+		autodocs: 'tag',
 	},
 };
